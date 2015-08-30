4910cc5780f0e3bc6db79386fae0fe8914be901d
suman <shumanghimiray@gmail.com>
2015-08-30 23:31:29 +0930

    added bipartite_graph.r

---
+++ bipartite_graph.r
@@ -0,0 +1,24 @@
+library(igraph)
+
+TestData <- read.table("test.txt", sep="\t", header=TRUE) #Loading data from external file.
+TestData$S4 <- ifelse(TestData$S3 >= mean(TestData$S3), 1, 0) #converting into dichotomous data.
+
+
+###Giving prefixes to the different nodes.
+myGraph <- graph.empty()
+myGraph <- add.vertices(myGraph,nv=length(unique(TestData$S1)),
+								attr=list(name=paste0('X',unique(TestData$S1)),
+                                type=rep(TRUE,length(unique(TestData$S1)))))
+								
+myGraph <- add.vertices(myGraph,nv=length(unique(TestData$S2)),
+								attr=list(name=paste0('Y',unique(TestData$S2)),
+                                type=rep(FALSE,length(unique(TestData$S2)))))
+
+##converting data to vector and using their names instead of index.
+myGraphVector <- as.vector(t(as.matrix(data.frame(S1=paste0('X',TestData$S1),
+                             S4=paste0('Y',TestData$S4)))))
+							 
+myGraph <- add.edges(myGraph,myGraphVector)
+##Plotting the graph
+plot.igraph(myGraph, layout=layout.bipartite,
+               vertex.color=c("green","cyan")[V(myGraph)$type+1])
